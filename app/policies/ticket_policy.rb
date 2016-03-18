6f33f7b18c74bb3b3dd46fb0001fcf7540027076
Jo Jangmi <wkdal720@gmail.com>
2016-03-18 13:54:10 -0700

    Only editors, managers and admins can create tickets

--- app/policies/ticket_policy.rb
+++ app/policies/ticket_policy.rb
@@ -8,6 +8,10 @@ class TicketPolicy < ApplicationPolicy
   def show?
     #right. because viewer doesn't have to be explicitly indicated.
     #since show action just needs any role.
-    user.try(:admin?) || record.project.roles.exists?(user_id: user)
+    user.try(:admin?) || record.project.has_member?(user)
+  end
+
+  def create?
+    user.try(:admin?) || record.project.has_manager?(user) || record.project.has_editor?(user)
   end
 end
